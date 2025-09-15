d32118c9b763194c0fc97673e9b2427aeaef1dd8
Andrejs Kristens Salkovs <a230539as@rvt.lv>
2025-09-15 14:03:50 +0300

    update

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,16 @@
 console.log('Page loaded');
 
 function homePage() {
-  console.log('Home Page opened');
-};
+    console.log('Home Page opened');
+    window.location.href = '../index.html';
+}
 
 function playersPage() {
-  console.log('Players Page opened');
-};
+    console.log('Players Page opened');
+    window.location.href = 'pages/playersPage.html';
+}
 
 function logInPage() {
-  console.log('Log In opened');
-};
+    console.log('Log In opened');
+    window.location.href = 'pages/logInPage.html';
+}
